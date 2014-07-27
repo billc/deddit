18035ef979c39ca23b13cd00b715d90d4c1b18f8
Andreas Ölund <andreas@dartvoid.com>
2014-07-28 01:11:28 +0200

    Added support to set which subreddit to fetch from

--- lib/deddit.dart
+++ lib/deddit.dart
@@ -1,10 +1,10 @@
 library deddit;
 
+import 'dart:async';
 import 'package:http/http.dart' as http;
 import 'package:json_object/json_object.dart';
 
 part 'item.dart';
-
 
 class Deddit {
   static Future<List> get(String subreddit) {
